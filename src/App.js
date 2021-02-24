f58a589ddc183845c52a31369a45ab1b15e8ee44
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-24 10:35:38 -0500

    examples of HOC and useContext via both funcion and class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Movie from './hoc/Movie';
+import React, { Component } from "react";
+import MoviePage from "./context/MoviePage";
+import UserContext from "./context/userContext";
+import CartContext from "./context/cartContext";
+import Login from "./context/Login";
 
-function App() {
-  return (
-    <Movie id={2} />
-  );
+class App extends Component {
+  handleLoggedIn = username => {
+    console.log("Getting the user: " + username);
+    const user = { name: "Mosh" };
+    this.setState({ currentUser: user });
+  };
+
+  state = { currentUser: null };
+
+  render() {
+    return (
+      <CartContext.Provider value={{ cart: [] }}>
+        <UserContext.Provider
+          value={{
+            currentUser: this.state.currentUser,
+            onLoggedIn: this.handleLoggedIn
+          }}
+        >
+          <div>
+            <MoviePage />
+            <Login />
+          </div>
+        </UserContext.Provider>
+      </CartContext.Provider>
+    );
+  }
 }
 
 export default App;
